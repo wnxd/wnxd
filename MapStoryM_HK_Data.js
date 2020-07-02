3a424de5c69e52f1865f90c0ec90589f5e2c4b3f
wnxd <imiku@wnxd.me>
2020-07-02 10:55:35 +0800

    Create MapStoryM_HK_Data.js

---
+++ MapStoryM_HK_Data.js
@@ -0,0 +1,8 @@
+var body = $response.body;
+var obj = JSON.parse(body);
+
+obj["errorCode"] = 0;
+obj["errorText"] = "1";
+obj["result"]["country"] = "HK";
+body = JSON.stringify(obj);
+$done(body);
