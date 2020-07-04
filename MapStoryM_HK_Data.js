bf7ea7aea8030720a800841f1b3fd00504b8ba57
wnxd <imiku@wnxd.me>
2020-07-04 22:56:40 +0800

    Update MapStoryM_HK_Data.js

--- MapStoryM_HK_Data.js
+++ MapStoryM_HK_Data.js
@@ -1,9 +1,13 @@
+var status = 'HTTP/1.1 200 OK';
+
+var headers = $response.headers;
+headers["errorCode"] = "0";
+
 var body = $response.body;
 var obj = JSON.parse(body);
-
 obj["errorCode"] = 0;
 obj["errorText"] = "1";
 obj["result"]["country"] = "HK";
-
 body = JSON.stringify(obj);
-$done(body);
+
+$done({status: status, headers: headers, body: body});
